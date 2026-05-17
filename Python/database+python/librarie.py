03110e068e2c41bff7a27ed2166bae546a9ca2b0
ucigaș de capibara <catalinzavtone@gmail.com>
2026-05-17 09:59:51 +0300

    ..

--- Python/database+python/librarie.py
+++ Python/database+python/librarie.py
@@ -1,7 +1,9 @@
-from sqlalchemy import MetaData, Table,Column,Integer ,String,create_engine,DateTime,text,ForeignKey,CheckConstraint
+from sqlalchemy import MetaData, Table,Column,Integer ,String
+from sqlalchemy import create_engine,DateTime,text,ForeignKey,CheckConstraint
+from sqlalchemy import insert
 from datetime import datetime
 metadata_obj= MetaData()
-engine = create_engine("sqlite:///librarie.db", echo=True)
+engine = create_engine("sqlite:///librarie.db")
 
 user_table=Table(
     "user_acount",
@@ -35,14 +37,15 @@ carte_table=Table(
     Column("stock",Integer,CheckConstraint("stock>=0"),nullable=False,default=0),
 
 )
-# autor_table=Table(
-#     "autor",
-#     metadata_obj,
-#     Column("id",Integer,primary_key=True,autoincrement=True),
-#     Column("Nume",String,nullable=False),
-#     Column("Prenume",String,nullable=False),
-#     Column("Tara",String,nullable=False),
-# )
+autor_table=Table(
+    "autor",
+    metadata_obj,
+    Column("id",Integer,primary_key=True,autoincrement=True),
+    Column("Nume",String,nullable=False),
+    Column("Prenume",String,nullable=False),
+    Column("Tara",String,nullable=False),
+    Column("carte_id",Integer,ForeignKey("carti.id"),nullable=False),
+)
 imprumut_table=Table(
     "imprumut",
     metadata_obj,
@@ -52,8 +55,43 @@ imprumut_table=Table(
     Column("Tara",String,nullable=False),
 )
 
-
-with engine.begin() as conn:
-    conn.execute(text('drop table autor'))
-
 metadata_obj.create_all(engine)
+def sterge_tabela():
+    nume=input("Introdu tabela pe care doresti so stergi")
+    with engine.begin() as conn:
+        conn.execute(text('drop table autor'))
+        conn.execute(text('drop table :y'), {"y": nume})
+
+def input_user():
+    while(True):
+        nume=input("Numele dumneavoastra:").strip().title()
+        prenume=input("Prenumele dumneavoastra:").strip().title()
+        email=input("Introdu emailul dumneavoastra:").strip().lower()
+        
+        if '@' in email and  nume and prenume:
+            break
+    try:
+            data_nasterii=input('Introdu data nasterii dumneavoastra:')
+            data_nasterii = datetime.strptime(data_nasterii,"%d-%m-%Y")
+            print("Succes! Formatul este corect:",data_nasterii)
+    except ValueError:
+            print("Eroare: Formatul specificat nu se potrivește cu textul.")
+    with engine.begin()as conn:
+        comanda=conn.execute(insert(user_table).values(Nume=nume,
+        Prenume=prenume,Data_nasterii=data_nasterii,
+        email=email))
+    
+
+input_user()
+    
+
+# def main():
+
+
+
+
+
+
+
+
+# main()
